9e3e372571f8b6bd247d830c0d4b55e67b661527
AlbertMart32 <amartinez3200@gmail.com>
2025-04-08 14:38:19 -0700

    vercel fix

--- data/databaseNeon.js
+++ data/databaseNeon.js
@@ -1,12 +1,10 @@
 import { neon } from "@neondatabase/serverless";
 
 // Neon database connection using environment variables
-if(!process.env.DATABASE_URL){
+if (!process.env.DATABASE_URL) {
   console.error("DATABASE_URL is not set in environment variables");
   process.exit(1);
 }
-const sql = neon(
-  process.env.DATABASE_URL
-);
-
+const sql = neon(process.env.DATABASE_URL);
+console.log("Connected to Neon database", process.env.DATABASE_URL);
 export default sql;
